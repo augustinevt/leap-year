e342ca3eb19c5b490d9d937b82c68530d156e4aa
Augustine von Trapp and George Olson <george@>
2016-06-08 08:58:06 -0700

    Add new conditional statements to business logic, implement result display on page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // Business Logic
 
 var leapYear = function(year){
-  if (year % 100 === 0){
+  if (isNaN(year) || (year <= 0)){
+    console.log("is not a valid year");
     return false;
-  } else if (year % 4 === 0){
+  } else if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
   } else {
     return false;
@@ -16,9 +17,19 @@ $(document).ready(function(){
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
 
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
+    var year = $("input#year").val();
+    var result = leapYear(parseInt(year));
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+
 
   });
 });
